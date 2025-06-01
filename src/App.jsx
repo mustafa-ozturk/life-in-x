f02b5d47cb20913ff782a497a82a9659fe7015ea
Mustafa Ozturk <68792671+mustafa-ozturk@users.noreply.github.com>
2025-06-01 17:39:49 -0400

    basic idea

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import styles from "./App.module.css";
+const DAYS_IN_YEAR = 365;
+const WEEKS_IN_YEAR = 52;
+const MONTHS_IN_YEAR = 12;
+const YEARS = 90;
 
 function App() {
-  return <>hello world</>;
+  return (
+    <>
+      hello world
+      <div className={styles.grid}>
+        <Square />
+        <Square filled={true} />
+      </div>
+    </>
+  );
+}
+
+function Square({ filled }) {
+  return (
+    <div
+      className={styles.square}
+      style={filled && { backgroundColor: "red" }}
+    ></div>
+  );
 }
 
 export default App;
